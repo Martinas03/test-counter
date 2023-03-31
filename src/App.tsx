56121b1586fabd0094aef6313e4775ab196a59eb
Martinas <martinas1990@mail.ru>
2023-03-31 17:20:47 +0300

    Developer/popup commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ export function App() {
             <Counter />
             <div>commit for dev 2</div>
             <div>commit for dev 3</div>
+            <div>Developer/popup commit 1</div>
         </div>
         )
 
