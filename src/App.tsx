15276df2d759de2edf8ac5cea7aa73d97e386dab
Martinas <martinas1990@mail.ru>
2023-03-31 17:22:25 +0300

    Developer/login commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ export function App() {
             <Counter />
             <div>commit for dev 2</div>
             <div>commit for dev 3</div>
+            <div>developer/login commit 1</div>
         </div>
         )
 
